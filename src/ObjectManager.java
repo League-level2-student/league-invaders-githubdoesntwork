8b88481b9d65512a757d9ca1ee5f2f5e04f9665a
League Student <league@iMac.attlocal.net>
2019-05-13 17:00:18 -0700

    extra thing

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,4 +1,23 @@
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ObjectManager {
-
+Rocketship rocket;
+Random r = new Random();
+ArrayList<Alien> aliens=new ArrayList<Alien>(); //create projetile
+public ObjectManager(Rocketship ship){
+	rocket = ship;
+}
+public void addAlien() {
+	aliens.add(new Alien(r.nextInt(500),0,50,50));
+}
+public void update() { //test Y
+	for (int i = 0; i < aliens.size(); i++) {
+		aliens.get(i).update();
+	}
+}
+public void draw(Graphics g) {
+	
+}
 }
